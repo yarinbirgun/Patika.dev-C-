63164273f4261ddff01e146d85968efafe79e547
Talha Mücahit ÖZBEK <86835874+yarinbirgun@users.noreply.github.com>
2021-07-22 18:03:24 +0300

    Add files via upload

---
+++ hata-yönetimi/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace hata_yönetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // try
+            // {
+            //     System.Console.WriteLine("Bir sayı giriniz:");
+            //     int sayi = Convert.ToInt32(Console.ReadLine());
+            //     System.Console.WriteLine("Girmiş oldugunuz sayı :" + sayi);
+            // }
+            // catch(Exception ex)
+            // {
+            //     Console.WriteLine("Hata: " + ex.Message.ToString());
+            // }
+            //finally
+            //{
+            //    System.Console.WriteLine("İşlem tamamlandı.");
+            //}
+            
+            try
+            {
+                //int a = int.Parse(null);
+                //int a = int.Parse("test");
+                int a = int.Parse("-20000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş deger giriniz");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun degil.");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük yada çok büyük bir deger girdiniz.");
+                Console.WriteLine(ex);
+            }
+            finally{
+                System.Console.WriteLine("İşlem başarıyla tamamlandı.");
+            }
+
+
+        }
+    }
+}
